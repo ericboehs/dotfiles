e44ae34d8513504a41501b72ebb63b3e2227a576
Eric Boehs <ericboehs@gmail.com>
2013-07-20 00:30:08 -0500

    Add global zsh alias "V" for |view -

--- shell/zsh/aliases.zsh
+++ shell/zsh/aliases.zsh
@@ -6,6 +6,7 @@ alias -g H='|head'
 alias -g W='|wc -l'
 alias -g S='|sort'
 alias -g K='|ruby -e "require %Q(open-uri); puts URI::encode STDIN.read" | while read i; do kiku $i; done'
+alias -g V='|view -'
 
 # Looks for "2 points" then sums STDIN on the index of the integer (column); only works for single digit points (0, 2, 4, 8)
 alias -g POINTS="|ruby -e 's=STDIN.to_a;i=s.grep(/point/)[0].index /[0-9]+ point/;p s.map{|l|l[i].to_i}.reduce(&:+)'"
