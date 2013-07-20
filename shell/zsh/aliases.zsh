6d778e9276020a93250d05b2e769fadef110b4c0
Eric Boehs <ericboehs@gmail.com>
2013-07-19 22:11:48 -0500

    Add POINTS global for calculating total points

--- shell/zsh/aliases.zsh
+++ shell/zsh/aliases.zsh
@@ -6,3 +6,7 @@ alias -g H='|head'
 alias -g W='|wc -l'
 alias -g S='|sort'
 alias -g K='|ruby -e "require %Q(open-uri); puts URI::encode STDIN.read" | while read i; do kiku $i; done'
+
+# Looks for "2 points" then sums STDIN on the index of the integer (column); only works for single digit points (0, 2, 4, 8)
+alias -g POINTS="|ruby -e 's=STDIN.to_a;i=s.grep(/point/)[0].index /[0-9]+ point/;p s.map{|l|l[i].to_i}.reduce(&:+)"
+
