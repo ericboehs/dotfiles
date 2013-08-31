afad232aec58d8ce81adad54a5ec5e89711a84f3
Eric Boehs <ericboehs@gmail.com>
2013-08-30 19:35:38 -0500

    Bundler 1.4 TURBO

--- shell/aliases/ruby.sh
+++ shell/aliases/ruby.sh
@@ -1,7 +1,7 @@
 # bundle exec
 alias b='bundle'
 alias be='bundle exec'
-alias bi='bundle install --path vendor/bundle --binstubs vendor/bundle/bin'
+alias bi='bundle install -j8 --path vendor/bundle --binstubs vendor/bundle/bin'
 
 # Switch ruby versions with homebrew. See: https://gist.github.com/ericboehs/5329013
 rbv () { brew switch ruby $(brew switch ruby list 2>&1 |tail -1|cut -d\  -f3-|tr -d ','|tr ' ' "\n" | grep "^$1" | head -1) && source ~/.zsh/config }
