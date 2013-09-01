a33278c4d5e8603992e05acce92fcebb9bffa7df
Eric Boehs <ericboehs@gmail.com>
2013-09-01 01:06:23 -0500

    Vi cursor in tmux/zsh

--- shell/zsh/vi-cursor.zsh
+++ shell/zsh/vi-cursor.zsh
@@ -1,9 +1,17 @@
 function zle-keymap-select zle-line-init
 {
     # change cursor shape in iTerm2
+    if [ -n "$TMUX" ]; then
+      COMMAND_CURSOR="\EPtmux;\E\E]50;CursorShape=0\C-G\E\\"
+      INSERT_CURSOR="\EPtmux;\E\E]50;CursorShape=1\C-G\E\\"
+    else
+      COMMAND_CURSOR="\E]50;CursorShape=0\C-G"
+      INSERT_CURSOR="\E]50;CursorShape=1\C-G"
+    fi
+
     case $KEYMAP in
-        vicmd)      print -n -- "\E]50;CursorShape=0\C-G";;  # block cursor
-        viins|main) print -n -- "\E]50;CursorShape=1\C-G";;  # line cursor
+        vicmd)      print -n -- $COMMAND_CURSOR;; # block cursor
+        viins|main) print -n -- $INSERT_CURSOR;;  # line cursor
     esac
 
     zle reset-prompt
