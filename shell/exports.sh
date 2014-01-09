4c0d5ec0a6772c831e1f4effc3715547bae90335
Eric Boehs <ericboehs@gmail.com>
2014-01-09 15:54:08 -0600

    Use RUBY_GC_HEAP_INIT_SLOTS instead of RUBY_HEAP_MIN_SLOTS

--- shell/exports.sh
+++ shell/exports.sh
@@ -22,7 +22,7 @@ export ANDROID_HOME=/usr/local/opt/android-sdk
 #export PAGER=less
 
 # Shaves about ~1s off Rails boot time
-export RUBY_HEAP_MIN_SLOTS=1000000
+export RUBY_GC_HEAP_INIT_SLOTS=1000000
 export RUBY_HEAP_SLOTS_INCREMENT=1000000
 export RUBY_HEAP_SLOTS_GROWTH_FACTOR=1
 export RUBY_GC_MALLOC_LIMIT=1000000000
