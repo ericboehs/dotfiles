56304bed62964af4a5d57fbf151f7338a0ca5599
Eric Boehs <ericboehs@gmail.com>
2014-02-01 03:53:52 -0600

    Add tb for ti build -p ios

--- shell/aliases/titanium.sh
+++ shell/aliases/titanium.sh
@@ -1,2 +1,3 @@
+tb() { ti build -p ios }
 tbd() { git config titanium.build.pp-uuid >& /dev/null && ti build -T device -C all -p ios -P "$(git config titanium.build.pp-uuid)" $@ || echo "Please run \`git config titanium.build.pp-uuid YOUR-PROFILE-UUID\`.\nThis is listed in \`ti info\`." }
 
