3538005af7effc034834b3e373e0da6283915291
Eric Boehs <ericboehs@gmail.com>
2014-02-01 02:58:54 -0600

    titanium build device (tbd) now informs user to set the PPID first

--- shell/aliases/titanium.sh
+++ shell/aliases/titanium.sh
@@ -1,1 +1,2 @@
-tbd() { ti build -T device -C all -p ios -P "$(git config titanium.build.pp-uuid)" $@ }
+tbd() { git config titanium.build.pp-uuid 1&>- && ti build -T device -C all -p ios -P "$(git config titanium.build.pp-uuid)" $@ || echo "Please run \`git config titanium.build.pp-uuid YOUR-PROFILE-UUID\`.\nThis is listed in \`ti info\`." }
+
