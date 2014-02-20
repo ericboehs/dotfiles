8a6f9fe47d7c57cd27b1ee5aa8a8998a993de2c0
Eric Boehs <ericboehs@gmail.com>
2014-02-20 13:36:47 -0600

    Add a device_udid option to tbd as titanium doesn't like "all" sometimes

--- shell/aliases/titanium.sh
+++ shell/aliases/titanium.sh
@@ -1,3 +1,9 @@
 tb() { ti build -p ios }
-tbd() { git config titanium.build.pp-uuid >& /dev/null && ti build -T device -C all -p ios -P "$(git config titanium.build.pp-uuid)" $@ || echo "Please run \`git config titanium.build.pp-uuid YOUR-PROFILE-UUID\`.\nThis is listed in \`ti info\`." }
+
+tbd() {
+  [[ -z $DEVICE_UDID ]] && DEVICE_UDID=$(git config titanium.build.device-udid)
+  git config titanium.build.pp-uuid >& /dev/null \
+    && ti build -T device -C "${DEVICE_UDID:-all}" -p ios -P "$(git config titanium.build.pp-uuid)" $@ \
+    || echo "Please run \`git config titanium.build.pp-uuid YOUR-PROFILE-UUID\`.\nThis is listed in \`ti info\`."
+}
 
