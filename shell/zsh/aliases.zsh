1491decad1e233088a0da790af5cbbab50fac7d1
Eric Boehs <ericboehs@gmail.com>
2014-06-19 14:44:17 -0500

    Update POINTS to work with new "Points: N" syntax

--- shell/zsh/aliases.zsh
+++ shell/zsh/aliases.zsh
@@ -8,8 +8,8 @@ alias -g W='|wc -l'
 alias -g S='|sort'
 alias -g V='|view -'
 
-# Looks for "2 points" then sums STDIN on the index of the integer (column); only works for single digit points (0, 2, 4, 8)
-alias -g POINTS="|ruby -e 's=STDIN.to_a;i=s.grep(/[^0] point/)[0]; i=i.index /[0-9]+ point/ if i;p !i ? 0 : s.map{|l|l[i].to_i}.reduce(&:+)'"
+# Looks for "Points: 2" from STDIN then sums capture from match
+alias -g POINTS="|ruby -e 'p STDIN.reduce(0){|sum, l| m = l.match /Points: ([0-9])+/; (m ? m.captures[0].to_i : 0) + sum}'"
 alias -g P=POINTS
 
 # Columnize ghi list output
