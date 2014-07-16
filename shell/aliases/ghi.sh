44484239b4f7cc3bc419a8ec2fd031d4d0b8541d
Eric Boehs <ericboehs@gmail.com>
2014-07-16 11:05:06 -0500

    Make the 'prs' and 'myprs' commands not filter by milestone

--- shell/aliases/ghi.sh
+++ shell/aliases/ghi.sh
@@ -2,8 +2,8 @@
 
 alias setcms='[[ -z $GHI_CURRENT_MILESTONE ]] && export GHI_CURRENT_MILESTONE=$( ghi milestone -S due_date -v | grep -E "Past due|^Due" -B5| grep -E "^\#" | tail -1 | cut -f1 -d:|tr -d "# \t\n") || true'
 
-function prs() { setcms; ghi list -M $GHI_CURRENT_MILESTONE $@ | grep '↑' --color=none }
-function myprs() { setcms; ghi list -M $GHI_CURRENT_MILESTONE --mine $@ | grep '↑' --color=none }
+function prs() { ghi list $@ | grep '↑' --color=none }
+function myprs() { ghi list --mine $@ | grep '↑' --color=none }
 
 alias ic='issues COL'
 function issues() { setcms; ghi list -M $GHI_CURRENT_MILESTONE $@ }
