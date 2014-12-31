a72dd014242b498d217b47b7766ff42ee589b298
Eric Boehs <ericboehs@gmail.com>
2014-12-31 02:42:06 -0600

    Add vim bindings to heroku rails console

--- shell/aliases/heroku.sh
+++ shell/aliases/heroku.sh
@@ -1,8 +1,11 @@
 # Heroku
 alias h='heroku'
+hc() { heroku run bash "-i -c 'curl -s https://raw.githubusercontent.com/ericboehs/dotfiles/master/config/inputrc > ~/.inputrc; rails c'" $@ }
+
 hs()  { heroku $@ -r staging }
 hsr() { heroku run $@ -r staging }
+hsc() { hc -r staging }
+
 hp()  { heroku $@ -r production }
 hpr() { heroku run $@ -r production }
-hq()  { heroku $@ -r qa }
-hqr() { heroku run $@ -r qa }
+hpc() { hc -r production }
