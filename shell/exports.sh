4960dcd791bbc9c6bbb75268503c41b020b63dde
Eric Boehs <ericboehs@gmail.com>
2015-01-30 19:21:25 -0600

    Add trello_cli

--- shell/exports.sh
+++ shell/exports.sh
@@ -26,3 +26,7 @@ export RUBY_HEAP_SLOTS_INCREMENT=1000000
 export RUBY_HEAP_SLOTS_GROWTH_FACTOR=1
 export RUBY_GC_MALLOC_LIMIT=1000000000
 export RUBY_HEAP_FREE_MIN=500000
+
+# For trello_cli. https://github.com/brettweavnet/trello_cli
+export TRELLO_DEVELOPER_PUBLIC_KEY=$(git config --get trello-cli.developer-public-key)
+export TRELLO_MEMBER_TOKEN=$(git config --get trello-cli.member-token)
