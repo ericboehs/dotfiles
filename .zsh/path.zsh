a2f121771dd3ee6f892d11a3fe05db3db2f83c21
Eric Boehs <ericboehs@gmail.com>
2015-08-05 12:10:10 -0500

    Add .bundle/bundle/bin to path

--- .zsh/path.zsh
+++ .zsh/path.zsh
@@ -3,6 +3,7 @@ typeset -U path
 
 # Add current directory's bin and vendor/bundle/bin directories to path safely
 path=(.git/safe/../../vendor/bundle/bin "$path[@]")
+path=(.git/safe/../../.bundle/bundle/bin "$path[@]")
 path=(.git/safe/../../bin "$path[@]")
 path=(~/bin "$path[@]")
 
