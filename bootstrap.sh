acd29f89fb4e4be1a6d3734a1e0ec5df7455ac37
Eric Boehs <eric@boehs.com>
2019-11-25 17:00:20 -0600

    Install asdf; install vim plugins

--- bootstrap.sh
+++ bootstrap.sh
@@ -54,19 +54,22 @@ done
 mkdir -p ~/.config
 ln -fs ~/.vim ~/.config/nvim
 
-echo "-----> Install asdf-vm"
-git clone https://github.com/asdf-vm/asdf.git ~/.asdf
-cd ~/.asdf
-git checkout "$(git describe --abbrev=0 --tags)"
+if [ -z ~/.asdf ];then
+  echo "-----> Install asdf-vm"
+  git clone https://github.com/asdf-vm/asdf.git ~/.asdf
+  cd ~/.asdf
+  git checkout "$(git describe --abbrev=0 --tags)"
 
-# Install node
-asdf plugin-add nodejs
-bash ~/.asdf/plugins/nodejs/bin/import-release-team-keyring
-#asdf install nodejs 8.12.0
-# Install ruby
-asdf plugin-add ruby
-asdf install ruby 2.6.5
-asdf global ruby 2.6.5
+  # Install node
+  asdf plugin-add nodejs
+  bash ~/.asdf/plugins/nodejs/bin/import-release-team-keyring
+  asdf install nodejs 8.12.0
+
+  # Install ruby
+  asdf plugin-add ruby
+  asdf install ruby 2.6.5
+  asdf global ruby 2.6.5
+fi
 
 # Install vim plugins
 vim -c ':silent !echo' -c ':PackUpdate' -c ':qa!'
