2801e1b3393c362aa8a68da689dbf2acec25e5a0
Eric Boehs <eric@boehs.com>
2023-02-08 16:30:18 -0600

    Change Pure's prompt color to green (when not a failure)

--- .zsh/pure.zsh
+++ .zsh/pure.zsh
@@ -1,4 +1,5 @@
 # `brew install pure` first
 fpath=( "/opt/homebrew/share/zsh/site-functions" $fpath )
 autoload -U promptinit && promptinit
+zstyle ':prompt:pure:prompt:success' color green
 prompt pure
