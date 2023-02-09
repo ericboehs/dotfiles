294f4dca86c42794bfd3b036c180e90e0ce8f117
Eric Boehs <eric@boehs.com>
2023-02-09 16:36:29 -0600

    Add dark theme for fzf

--- .zsh/fzf.zsh
+++ .zsh/fzf.zsh
@@ -1,4 +1,9 @@
 # Check out https://minsw.github.io/fzf-color-picker/
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
 
-export FZF_DEFAULT_OPTS=$FZF_DEFAULT_OPTS' --color=fg:#000000,bg:#ffffff,hl:#5f87af --color=fg+:#ffffff,bg+:#0449b6,hl+:#aeebe9 --color=info:#7600ad,prompt:#0449b6,pointer:#ffffff --color=marker:#a1a1a1,spinner:#af5fff,header:#008000'
+# Light theme
+# export FZF_DEFAULT_OPTS=$FZF_DEFAULT_OPTS' --color=fg:#000000,bg:#ffffff,hl:#5f87af --color=fg+:#ffffff,bg+:#0449b6,hl+:#aeebe9 --color=info:#7600ad,prompt:#0449b6,pointer:#ffffff --color=marker:#a1a1a1,spinner:#af5fff,header:#008000'
+
+# Dark theme
+export FZF_DEFAULT_OPTS=$FZF_DEFAULT_OPTS' --color=fg:#f8f8f2,bg:#26292E,hl:#bd93f9 --color=fg+:#f8f8f2,bg+:#44475a,hl+:#bd93f9 --color=info:#ffb86c,prompt:#50fa7b,pointer:#ff79c6 --color=marker:#ff79c6,spinner:#ffb86c,header:#6272a4'
+
