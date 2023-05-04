8ef8e20f7c2958598d4b4ee6bce2600f37096dad
Eric Boehs <eric@boehs.com>
2023-05-03 19:34:21 -0500

    Bring back Ctrl-L for navigating panes
    I have escape in vim configured to :noh|echo<cr> and if I really need to
    send Ctrl-L I can use <Prefix>Ctrl-L.

--- .zsh/keybindings.zsh
+++ .zsh/keybindings.zsh
@@ -4,3 +4,6 @@ bindkey -e
 # Open vim when pressing `v` in normal mode (Press Ctrl-X, Ctrl-V to temporarily enter vi's normal mode)
 autoload edit-command-line; zle -N edit-command-line
 bindkey -M vicmd v edit-command-line
+
+# Bind the Esc key to clear the screen
+bindkey '\e' clear-screen
