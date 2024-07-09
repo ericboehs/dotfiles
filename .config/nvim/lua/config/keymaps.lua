de9cf0d633411f9f7fdcc16d23869d19ea325d3f
Eric Boehs <eric@boehs.com>
2024-07-09 11:57:58 -0500

    LazyVim: Add Ctrl-q, remove M-j/k, don't auto update plugins

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -2,6 +2,10 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
+-- Ctrl-q to Quit
+vim.keymap.set("n", "<C-q>", ":q<CR>")
+vim.keymap.set("i", "<C-q>", "<Esc>:q<CR>")
+
 -- Tmux Navigator
 vim.keymap.set("n", "<C-h>", "<Cmd>NvimTmuxNavigateLeft<CR>", { silent = true })
 vim.keymap.set("n", "<C-j>", "<Cmd>NvimTmuxNavigateDown<CR>", { silent = true })
@@ -13,3 +17,7 @@ vim.keymap.set("n", "<leader>ta", "<Cmd>VtrAttachToPane<CR>", { silent = true })
 vim.keymap.set("n", "<leader>tc", "<Cmd>VtrSendCommandToRunner<CR>", { silent = true })
 vim.keymap.set("n", "<leader>tl", "<Cmd>VtrSendLinesToRunner<CR>", { silent = true })
 vim.keymap.set("v", "<leader>tl", "<Cmd>VtrSendLinesToRunner<CR>", { silent = true })
+
+-- Disable moving lines via <M-j> and <M-k>
+vim.keymap.set("n", "<M-j>", "j")
+vim.keymap.set("n", "<M-k>", "k")
