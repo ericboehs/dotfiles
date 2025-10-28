eb0010f85f0af7b681f546e63f65440251c31d0f
Eric Boehs <eric@boehs.com>
2025-10-28 16:14:15 -0500

    Update neovim plugin configuration
    - Update LazyVim to version 8
    - Enhance CSV plugin with auto-arrange and keybinding support
    - Fix mini.pairs plugin path
    - Re-enable Octo.nvim plugin for GitHub integration

--- .config/nvim/lua/plugins/csv.vim.lua
+++ .config/nvim/lua/plugins/csv.vim.lua
@@ -1,4 +1,14 @@
 return {
   'chrisbra/csv.vim',
-  ft = 'csv',
+  ft = { 'csv', 'dat' },
+  config = function()
+    -- Ensure filetype plugins are enabled
+    vim.cmd('filetype plugin on')
+
+    -- Set CSV plugin options
+    vim.g.csv_autocmd_arrange = 1
+    vim.g.csv_autocmd_arrange_size = 1024*1024
+
+    vim.g.csv_bind_B = 1
+  end,
 }
--- .config/nvim/lua/plugins/octonvim.lua
+++ .config/nvim/lua/plugins/octonvim.lua
@@ -1,11 +1,13 @@
 return {
-  -- "pwntester/octo.nvim",
-  -- requires = {
-  --   "nvim-lua/plenary.nvim",
-  --   "nvim-telescope/telescope.nvim",
-  --   "nvim-tree/nvim-web-devicons",
-  -- },
-  -- config = function()
-  --   require("octo").setup()
-  -- end,
+  "pwntester/octo.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-telescope/telescope.nvim",
+    "nvim-tree/nvim-web-devicons",
+  },
+  cmd = "Octo",
+  event = "VeryLazy",
+  config = function()
+    require("octo").setup()
+  end,
 }
